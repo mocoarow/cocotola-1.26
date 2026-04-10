f4683dcf1b989faa2a6181ebdb4816bcdb3903fa
mocoarow <mocoarow@gmail.com>
2026-04-10 21:03:01 +0900

    refactor: remove unused SafeIntToInt32 and GetIntFromPath helpers

--- cocotola-auth/controller/handler/helper.go
+++ cocotola-auth/controller/handler/helper.go
@@ -1,10 +1,6 @@
 package handler
 
 import (
-	"fmt"
-	"math"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/mocoarow/cocotola-1.26/cocotola-auth/controller"
@@ -23,23 +19,4 @@ func GetAppUserIDFromContext(c *gin.Context) (domain.AppUserID, bool) {
 		return domain.AppUserID{}, false
 	}
 	return id, !id.IsZero()
-}
-
-// GetIntFromPath extracts an integer value from the URL path parameter with the given name.
-func GetIntFromPath(c *gin.Context, param string) (int, error) {
-	idS := c.Param(param)
-	id, err := strconv.Atoi(idS)
-	if err != nil {
-		return 0, fmt.Errorf("convert string to int(%s): %w", idS, err)
-	}
-
-	return id, nil
-}
-
-// SafeIntToInt32 converts an int to int32 with overflow check.
-func SafeIntToInt32(v int) (int32, error) {
-	if v < math.MinInt32 || v > math.MaxInt32 {
-		return 0, fmt.Errorf("value %d overflows int32", v)
-	}
-	return int32(v), nil
 }
